589ee4c146ff1ef1a003ca18e8fa20ce89fbe558
Son Trinh <sontrinh179@gmail.com>
2021-01-27 16:07:16 +0700

    Add layout for header + Theme layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import './App.css';
+import Header from './components/Header';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }),
-);
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#673ab7',
+    }
+  },
+});
 
-function App() {
-  const classes = useStyles();
-
+const App = () => {
   return (
-    <div className={classes.root}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login / Register</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Header />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
